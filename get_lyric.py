5a075795a36508d9189eb407c363a62fc736c649
zyme <lolrichyisswag@gmail.com>
2022-10-02 01:53:50 -0500

    technically, fully works now?

--- get_lyric.py
+++ get_lyric.py
@@ -4,17 +4,20 @@ import lyricsgenius
 from dotenv import load_dotenv
 import os
 import sys
+import re
 
 load_dotenv(".env")
 g = lyricsgenius.Genius(os.getenv("APIKEY")) # Puts it in a secure place
 g.verbose = False
 artist = g.search_artist(sys.argv[4], max_songs=1, sort="title")
-track = g.search_song(sys.argv[2], artist.name)
+track = g.search_song(sys.argv[2], artist.name, get_full_info=True)
 
 if artist is None:
     quit()
 if track is None:
     quit() 
 else:
-    print(track.lyrics)
+    result = re.sub('^(.+Lyrics)', "", track.lyrics)
+    result = re.sub('(\d.+Embed)$', "", track.lyrics)
+    print(result)
     
